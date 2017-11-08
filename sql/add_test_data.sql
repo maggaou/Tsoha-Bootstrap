126a26640a2ce6977ef90149e7786c45ea30f1e7
Magga Oula P <magga@vdi-cubbli-023.ad.helsinki.fi>
2017-11-08 20:36:34 +0200

    sql-komennot tehty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,15 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kategoria (nimi) VALUES 
+	('Opiskelu'),
+	('Vapaa-aika'),
+	('Urheilu')
+;
+INSERT INTO Käyttäjä (nimi,salasana,asema) VALUES 
+	('jokke','jokke1234','opiskelija'),
+	('anna','anna1234','opiskelija'),
+	('jussi','jussi1234','vastuuhenkilö')
+;
+INSERT INTO Aihe (nimi) VALUES 
+	('Muistilista'),
+	('Juoksupäiväkirja'),
+	('Oppimispäiväkirja')
+;
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,24 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kategoria (
+	kategoria_id SERIAL PRIMARY KEY,
+	nimi varchar(100)
+);
+
+CREATE TABLE Aihe (
+	aihe_id SERIAL PRIMARY KEY,
+	nimi varchar(100),
+	kuvaus varchar(2000)
+);
+
+CREATE TABLE KategoriaAihe (
+	kategoria_id int REFERENCES Kategoria(kategoria_id),
+	aihe_id int REFERENCES Aihe(aihe_id)
+);
+
+CREATE TABLE Käyttäjä (
+	käyttäjä_id SERIAL PRIMARY KEY,
+	nimi varchar(30),
+	salasana varchar(30),
+	asema varchar(30),
+	aihe_id int REFERENCES Aihe(aihe_id)
+);
+	
